51c16bee366285e9cf07f74c7a798e7e79e3ad9f
veronj <veron_j@etna-alternance.net>
2019-05-22 20:36:26 +0200

    models

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\book;
+use Faker\Generator as Faker;
+
+$factory->define(book::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
